f53b15f5f257b8c60703e6066034e5048c5a5f13
huxiaoou <516984451@qq.com>
2025-05-23 10:32:53 +0800

    Add factor REOC

--- math_tools/robust.py
+++ math_tools/robust.py
@@ -27,3 +27,15 @@ def robust_ret_log(x: pd.Series, y: pd.Series, scale: float = 1.0) -> pd.Series:
     :return: for log return, x, y are supposed to be positive
     """
     return (np.log(x.where(x > 0, np.nan) / y.where(y > 0, np.nan))) * scale
+
+
+def robust_div(x: pd.Series, y: pd.Series, nan_val: float = np.nan) -> pd.Series:
+    """
+
+    :param x: must have the same length as y
+    :param y:
+    :param nan_val:
+    :return:
+    """
+
+    return (x / y.where(y != 0, np.nan)).fillna(nan_val)
